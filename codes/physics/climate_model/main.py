ca34d9f42d7162509a48efb3b6a971f1211b20ec
reneaas <reneaas@uio.no>
2023-07-06 07:58:18 +0200

    updated website

---
+++ codes/physics/climate_model/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+def main():
+    stefan_boltzmann_constant = 5.67e-8
+    solar_constant = 1360
+    albedo = 0.28
+    
+
--- test.py
+++
@@ -1,4 +0,0 @@
-import os
-
-url = "https://raw.githubusercontent.com/reneaas/vgs_programmering/main/datasets/IO_data/x_2.txt"
-os.system(" ".join(["curl", url, "-o", "x_2.txt"]))
